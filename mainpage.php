e2b447dba51dc9c66f59601206c80f5b92f7cc91
Geviktor <gevgeviktor@gmail.com>
2022-04-11 09:57:51 +0300

    file download and active users added

--- mainpage.php
+++ mainpage.php
@@ -1,40 +1,51 @@
 <?php
     session_start();
     require_once "dbconfig.php";
+    date_default_timezone_set('Europe/Istanbul');
     $ownedFiles = $othersFiles = array();
 
-    echo 'You are welcome ' . htmlspecialchars($_SESSION["username"]);
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         header("location: login.php");
         exit;
     }
+    echo 'You are welcome ' . htmlspecialchars($_SESSION["username"]);
+
+    $id = $_SESSION["id"];
+    $timeCheck=time()-300;
+    $time=date("Y-m-d H:i:s",$timeCheck+600);
+
+    $sql="UPDATE users SET last_active='$time' WHERE id = $id";
+    $run = mysqli_query($conn, $sql);
+
 
     if($_SESSION["auth"]==1){
-        $id = $_SESSION["id"];
         $sql = "SELECT users.username ,files.filename, files.created_at FROM users, files WHERE users.id = files.owner_id AND (users.id = $id OR users.auth = 0 )";
     } else{
-        $id = $_SESSION["id"];
         $sql = "SELECT users.username ,files.filename, files.created_at FROM users, files WHERE users.id = files.owner_id AND (users.id = $id OR users.auth = 1)";
     }
-
     $result = mysqli_query($conn, $sql);
+
     if(!empty($result)){
         while ($row = mysqli_fetch_assoc($result)){
             if($row["username"]==$_SESSION["username"]){
-                $line = $row["filename"] . "  |  " . $row["created_at"] . "<br>";
+                $line =  $row["filename"] . "  |  " . $row["created_at"] . " <a href='uploads/" . $row["filename"] . "'>Download </a><br>";
                 array_push($ownedFiles, $line);
             } else{
-                $line = $row["username"] . "  |  " . $row["filename"] . "  |  " . $row["created_at"] . "<br>";
+                $line = $row["username"] . "  |  " . $row["filename"] . "  |  " . $row["created_at"] . " <a href='uploads/" . $row["filename"] . "'>Download </a><br>";
                 array_push($othersFiles, $line);
             }
         }
     }
+
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
 <body>
     <?php
+        echo "<hr>";
         if(!empty($ownedFiles)){
             echo "<h2>Your Files</h2>";
             echo "<h4>Filename | Upload time</h4>";
@@ -49,25 +60,39 @@
                 echo $line;
             }
         }
+        echo "<hr>";
     ?>
-<!--
-    #    if(mysqli_num_rows($result)>0){
-     #       while ($row = mysqli_fetch_assoc($result)){
-    #            printf("%s    %s  |   %s\n", $row["username"], $row["filename"], $row["created_at"]);
-    #        }
-    #    }
--->
+
     <h2>UPLOAD FILE FROM HERE</h2>
     <form method="post" action="upload.php" enctype="multipart/form-data">
         <input type="file" name="upload" />
         <input type="submit" value="Upload">
     </form>
     <?php
-     if(isset($_SESSION["uploaded"])){
+    if(isset($_SESSION["uploaded"])){
         echo "<br>" . $_SESSION["uploaded"];
     }
+
+    echo "<hr>";
+
+    if($_SESSION["auth"]==1){
+        echo "<h3>USERS</h3>";
+        $sql = "SELECT username,last_active FROM users ORDER BY last_active DESC";
+        $result = mysqli_query($conn, $sql);
+
+        while ($row = mysqli_fetch_assoc($result)){
+            if (strtotime($row["last_active"])>$timeCheck){
+                echo $row["username"] . " |  Online now<br>";
+            } else {
+                echo $row["username"] . ' |  Last active at ' . $row["last_active"] . "<br>";
+            }
+        }
+        echo "<hr>";
+    }
     ?>
-    <hr>
     <a href="/logout.php"><h3>Log OUT!</h3></a>
+
+    <script>
+    </script>
 </body>
 </html>
